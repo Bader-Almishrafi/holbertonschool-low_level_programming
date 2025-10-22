8583ad20d9f87df41dd5b87f0b7595a4f5f13a85
Bader-Almishrafi <baderalmishrafi@gmail.com>
2025-10-22 13:50:06 +0000

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,19 +2,17 @@
 
 /**
  * print_rev - Prints a string in reverse.
- * @str: The string to be printed.
+ * @s: The string to be printed.
  */
-void print_rev(char *str)
+void print_rev(char *s)
 {
     int len = 0, index;
 
-    // Start index from 0
-    for (index = 0; str[index] != '\0'; index++)
+    for (index = 0; s[index] != '\0'; index++)
         len++;
 
-    // Print characters in reverse
     for (index = len - 1; index >= 0; index--)
-        _putchar(str[index]);
+        _putchar(s[index]);
 
     _putchar('\n');
 }
