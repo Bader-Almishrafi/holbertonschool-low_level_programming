085bee05e5111757a85f3f8f4649e8047a006cad
Bader-Almishrafi <baderalmishrafi@gmail.com>
2025-12-01 14:41:32 +0000

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,31 +8,29 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new node.
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new, *temp;
+	dlistint_t *new, *last;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-    if (*head == NULL)
-    {
-        new->prev = NULL;
-        *head = new;
-        return (new);
-    }
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
 
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
 
-    temp->next = new;
-    new->prev = temp;
-
-    return (new);
+	return (new);
 }
